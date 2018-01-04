6bbf6c2ffdfdd63f9fad51fb454999e66c02396d
alevegap92 <ravp_92@hotmail.com>
2018-01-04 00:30:59 -0300

    2.3 - create ModelViewSet UF

--- uf/views.py
+++ uf/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import viewsets
+from uf.models import *
+from uf.serializers import *
+
+class UfViewSet(viewsets.ModelViewSet):
+
+    queryset = Uf.objects.all()
+    serializer_class = UfSerializer
